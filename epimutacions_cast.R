a28b0aa650a80b8d69388b80adc20f29a17ef09f
Xavier <xavier.escriba.montagut@gmail.com>
2022-11-17 15:50:29 +0100

    last update

--- epimutacions_cast.R
+++ epimutacions_cast.R
@@ -1,4 +1,4 @@
-## Cargamos la libreria
+## Cargamos la librería
 library(epimutacions)
 
 ## Cargamos un paquete que nos permite acceder a los datasets y cargamos
@@ -8,17 +8,36 @@ eh <- ExperimentHub()
 query(eh, c("epimutacionsData"))
 methy <- eh[["EH6690"]]
 
+## Exploramos la estructura del GenomicRatioSet
+methy
+assays(methy)$Beta[1:5,1:5]  ## Betas (nivel de metilación)
+pData(methy)  ## Fenotipos (edad, sexo, status)
+
 ## Cargamos los datos que usaremos para el diseño leave-one-out
 data(GRset)
+GRset
+assays(GRset)$Beta[1:5,1:5]  ## Betas (nivel de metilación)
+pData(GRset)  ## Fenotipos (edad, sexo, status)
 
-
-## ---- La normalizacion debe hacerse de manera independiente
+## La normalizacion debe hacerse de manera independiente
 ## pero hemos implementado una normalizacion conjunta que se 
 ## puede hacer de la siguiente manera (costoso computacionalmente):
 ## NO CORRER 
 ## Dataset_procesado <- epi_preprocess(directorio,
 ##                                   panel_referencia,
 ##                                   pattern = "SampleSheet.csv")
+
+
+## Esta función nos permite cambiar los parámetros de cualquiera de los métodos.
+## En la user's guide hay una tabla de referencia con la explicación de los
+## parámetros modificables. Con este comando vemos los valores actuales:
+epi_parameters()
+
+
+## Cambiamos el p-valor umbral que determina qué regiones son
+## outliers en el método manova:
+parameters <- epi_parameters(manova = list("pvalue_cutoff" = 0.01))
+parameters
 
 
 ## Diseño caso-control
@@ -39,61 +58,31 @@ epi_mvo_one_leave_out<- epimutations_one_leave_out(GRset,
                                                    method = 'manova')
 
 
-## Esta función nos permite cambiar los parámetros de cualquiera de los métodos.
-## En la user's guide hay una tabla de referencia con la explicación de los
-## parámetros modificables. Con este comando vemos los valores actuales:
-epi_parameters()
-
-
-## Cambiamos el p-valor umbral que determina qué regiones son
-## outliers en el método manova:
-parameters$manova$pvalue_cutoff 
-parameters <- epi_parameters(manova = list("pvalue_cutoff" = 0.01))
-parameters$manova$pvalue_cutoff 
-
 
 ## Accedemos a los resultados:
 
 ## Dimensiones de la tabla - 51 líneas(epimutaciones) x 16 columnas (características)
 dim(epi_mvo)
-class(epi_mvo)
 ## Vemos las características de las primeras 12 epimutaciones
-head(as.data.frame(epi_mvo), 12)
-
-## Para las epimutaciones de la 10 a la 15, consultamos el nombre, el 
-## número de CpGs, el p-valor y el p-valor ajustado
-as.data.frame(epi_mvo)[c(11:16),c("epi_id","cpg_n","pvalue","adj_pvalue")]
-
+head(epi_mvo, 12)
 
 ## Anotación de las epimutaciones, para añadir información extra
-rst_mvo <- annotate_epimutations(epi_mvo, omim = FALSE)
+rst_mvo <- annotate_epimutations(epi_mvo)
 
 
-## Para ver la tabla con los datos anotados, podemos acceder a través de 
-## los nombres de las columnas
-rst_mvo[ c(27,32) ,c("epi_id", "cpg_ids", "Relation_to_Island")]
-rst_mvo[ c(1:5), c("epi_id", "ensembl_reg_id", "ensembl_reg_type")]
-rst_mvo[ c(1:5), c("GencodeBasicV12_NAME", "epi_id", "ensembl_reg_id", "ensembl_reg_type")]
-
-
-## Grafcamos una epimutación en concreto (en este caso, la 15)
-plot_epimutations(as.data.frame(epi_mvo[15,]), methy)
+## Graficamos una epimutación en concreto (en este caso, la 15)
+plot_epimutations(epi_mvo[epi_mvo$epi_id == 'epi_manova_1',], methy)
 
 
 ## Graficamos la 1a epimutación con anotación de genes
-plot_epimutations(as.data.frame(epi_mvo[1,]), 
-                       methy = methy, 
-                       genes_annot = TRUE)
+plot_epimutations(as.data.frame(epi_mvo[epi_mvo$epi_id == 'epi_manova_1',]), 
+                  methy = methy, 
+                  genes_annot = TRUE)
 
 
 
 ## Graficamos la 1a epimutación con anotación de metilación y acetilación
 ## H3K4Me3, H3K27Ac y H3K27Me3
-plot_epimutations(as.data.frame(epi_mvo[1,]), 
-                       methy =  methy, 
-                       regulation = TRUE)
-
-
-
-## Finalmente, para correr los mismos análisis con la ayuda de una interfaz gráfica:
-epimutacionsShiny::run_app()
+plot_epimutations(as.data.frame(epi_mvo[epi_mvo$epi_id == 'epi_manova_1',]), 
+                  methy =  methy, 
+                  regulation = TRUE)
